000bc5b88d4634ec5e22be11ced0b3e7e04b6a9d
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-06-28 22:49:51 +0100

    Added example tester script.

---
+++ scripts/blindex-test.sh
@@ -0,0 +1,1 @@
+java -jar target/blindex-*jar-with*.jar src/test/resources/newspapers-jisc2/table_sample_1.csv http://localhost:8080/discovery "file://$PWD/src/test/resources/newspapers-jisc2/" .xml
