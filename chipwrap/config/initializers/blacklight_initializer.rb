24a0e38068ad3436480d09609e1104d878bf7d87
Andrew Jackson <Andrew.Jackson@bl.uk>
2014-10-24 10:46:30 +0100

    Added Blacklight test.

---
+++ chipwrap/config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = '549550dda3f5367cee845841614ab794423e30d7c31678db8ca587c28a9a559a7831f7bd84ad2b84dd09a892a2319f17b9af99b2cfea5cfbc0cb0d343962853d'
+
